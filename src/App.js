f30ffaaa80c1fc237bb0c34df9e10f6742c9ca6d
sachinpasi <61353974+sachinpasi@users.noreply.github.com>
2021-01-05 22:27:30 +0530

    tinder-clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Header";
+import TinderCard from "./Tinder_Cards";
+import SwipeButtons from "./SwipeButtons";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //BEM class naming convection
+
+    <div className="app">
+      <Header />
+      <TinderCard />
+      <SwipeButtons />
     </div>
   );
 }
